7944e0760c222ba2f63e6d069b530aff400594dd
snehalrakhondwe1997 <97503557+snehalrakhondwe1997@users.noreply.github.com>
2022-05-01 20:46:54 -0700

    Add files via upload

---
+++ AP17.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script type="text/javascript" src="message.js"></script>
+</head>
+<body>
+<p>Welcome to javaScript</p>
+<form>
+<input type="button" value="click" onclick="msg()"/>
+</form>
+
+
+</body>
+</html>
